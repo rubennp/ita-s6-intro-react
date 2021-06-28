087a00a5fd6bff160cd4cfb62a4b9503940e112d
Rubèn Nieto <ruben.np@me.com>
2021-06-28 11:26:43 +0200

    Fi ex3

--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,6 +1,13 @@
 const Escena = (props) => {
+    const styles = {
+        textAlign: "center",
+        border: "1px solid black",
+        borderRadius: "20px",
+        padding: "10px",
+    }
+    
     return (
-        <p>{props.frase}</p>
+        <p style={styles}>{props.frase}</p>
     );
 };
 

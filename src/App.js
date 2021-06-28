7a570179c1bca4fad41a2239f76d12b5d2a7dd04
Rubèn Nieto <ruben.np@me.com>
2021-06-28 12:36:57 +0200

    Fi Ex4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import React, { useState } from "react";
+
 import Escena from "./components/escena/Escena";
+import Boto from "./components/escena/Boto";
 
 import frases from "./assets/data";
 
 // import './css/App.css';
 
 const  App = () => {
+  const [pos, setPos] = useState(0);
+
+  const
+  
+  ulStyles = {
+    listStyleType: "none",
+    padding: "0",
+    margin: "0",
+  },
+  mainStyles = {
+    padding: "0 20px",
+  };
+
+  const antFrase = () => setPos(pos === 0 ? frases.length-1 : pos-1);
+  const segFrase = () => setPos(pos === frases.length-1 ? 0 : pos+1);
+
   return(
     <div>
-      {frases.map((frase, index) => <Escena key={index} frase={frase}/>)}
+      <ul style={ulStyles}>
+        <Boto key="0" func="Anterior" hFunc={antFrase}/>
+        <Boto key="1" func="Següent" hFunc={segFrase}/>
+      </ul>
+      <main style={mainStyles}>
+        {frases.map((frase, index) => <Escena key={index} frase={frase} actiu={pos === index}/>)}
+      </main>
     </div>
   );
 };
--- src/components/escena/Escena.js
+++ src/components/escena/Escena.js
@@ -1,11 +1,13 @@
 const Escena = (props) => {
     const styles = {
         textAlign: "center",
-        border: "1px solid black",
+        border: "2px solid black",
         borderRadius: "20px",
         padding: "10px",
-    }
+    };
     
+    if (props.actiu) styles.backgroundColor = "rgba(255, 0, 0, 0.5)";
+
     return (
         <p style={styles}>{props.frase}</p>
     );
